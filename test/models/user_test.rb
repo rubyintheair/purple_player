a76ecd65fb09e6638ddd2ffa0ffdaf5283e4aa7a
Quy <quy.nguyenngoctp@gmail.com>
2017-11-22 22:03:20 +0700

    basic test for User: password, password_confirmation, save user, method authenticate

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -19,10 +19,54 @@ class UserTest < ActiveSupport::TestCase
     assert_equal "Lalala", a.last_name
   end
 
-  test "should not save without email" do 
-    a = User.new 
-    assert_not a.save 
-    assert_equal ["can't be blank"], a.errors[:first_name]
+  test "should allow setting password" do 
+    a = User.new(first_name: "Adam")
+    a.password = "asdf"
+    assert_equal "asdf", a.password
+  end
+
+  test "should not save without password" do 
+    a = User.create(first_name: "Quy", last_name: "Nguyen")
+    assert_equal ["can't be blank"], a.errors[:password]
+  end
+
+  test "should allow setting password confirmation" do 
+    a = User.new(first_name: "Quy", last_name: "Nguyen")
+    a.password = "123"
+    a.password_confirmation = "123"
+    assert_equal "123", a.password_confirmation
   end 
+
+  test "should save with a password but without confirmation" do 
+    a = User.create(first_name: "Quy", last_name: "Nguyen", password: "123")
+    assert a.save 
+  end 
+
+  test "should not save with incorrect password_confirmation" do 
+    a = User.new(first_name: "Quy", last_name: "Nguyen")
+    a.password = "123"
+    a.password_confirmation = "000"
+    a.save 
+    assert_equal ["doesn't match Password"], a.errors[:password_confirmation]
+  end 
+
+  test "should save with correct password confirmation" do 
+    a = User.new(first_name: "Quy", last_name: "Nguyen", password: "123")
+    a.password_confirmation = "123"
+    assert a.save 
+  end 
+
+  test "authenticate should return true if password matches" do 
+    a = User.create!(first_name: "Quy", last_name: "Nguyen", password: "123")
+    assert a.save 
+    assert_not a.authenticate("must understand authenticate")
+    assert a.authenticate("123")
+  end 
+
+  # test "should not save without email" do 
+  #   a = User.new 
+  #   assert_not a.save 
+  #   assert_equal ["can't be blank"], a.errors[:first_name]
+  # end 
 
 end
